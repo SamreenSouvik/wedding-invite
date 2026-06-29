1704e9cb5269fcce258eecfaddf9df23efc3a5b3
Souvik Dandapat <dandapat@Souviks-MacBook-Air.local>
2026-06-29 09:35:18 +0530

    Added website template Claude

---
+++ FirstPage/Marriage Invitation Website _ Use AI_files/0m80gouyj5z~h.js
@@ -0,0 +1,5 @@
+;!function(){try { var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof global?global:"undefined"!=typeof window?window:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&((e._debugIds|| (e._debugIds={}))[n]="5b78ad16-4714-be88-13fd-bc1cd12926c1")}catch(e){}}();
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,688953,o=>{o.n(o.i(794441))}]);
+
+//# debugId=5b78ad16-4714-be88-13fd-bc1cd12926c1
+//# sourceMappingURL=14i6ru5ij3guz.js.map
